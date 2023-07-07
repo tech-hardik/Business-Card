2ae146a8fb4140144d953cd4b218eaf9a780f579
HARDIK SHARMA <hardik.tech@outlook.com>
2023-07-07 14:03:19 +0530

    Business Card App Completed.

--- Business Card/ContentView.swift
+++ Business Card/ContentView.swift
@@ -9,13 +9,41 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+        ZStack{
+            Color("darkGreen")
+                .ignoresSafeArea()
+            VStack {
+                Image("805319BA-8994-4F21-8FC2-0DD303018F17")
+                    .resizable()
+                    .frame(width:350, height:450)
+                    .clipShape(Capsule())
+                Text("Hardik Sharma")
+                    .foregroundColor(.white)
+                    .bold()
+                    .font(.title)
+                
+                Text("iOS Developer")
+                    .foregroundColor(.white)
+                    .bold()
+                    .font(.title2)
+                
+                Text("BTech IT Student at MUJ")
+                    .foregroundColor(.white)
+                    .italic()
+                    .font(.title3)
+                
+                RoundedRectangle(cornerRadius:15)
+                    .frame(width:350,height:50)
+                    .foregroundColor(.white)
+                    .overlay{
+                        HStack{
+                            Image(systemName: "mail")
+                            Text("hardik.tech@outlook.com")
+                                .font(.title2)
+                        }
+                    }
+            }
         }
-        .padding()
     }
 }
 
