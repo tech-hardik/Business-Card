a8df3c89b69c35048101336efb502e27972b024b
HARDIK SHARMA <hardik.tech@outlook.com>
2023-07-07 11:29:42 +0530

    Initial Commit

---
+++ Business Card/Business_CardApp.swift
@@ -0,0 +1,17 @@
+//
+//  Business_CardApp.swift
+//  Business Card
+//
+//  Created by HARDIK SHARMA on 07/07/23.
+//
+
+import SwiftUI
+
+@main
+struct Business_CardApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
